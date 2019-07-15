57723ae8cf26b2a95e78e3ccb35b7423a40151fc
Didik Kurniawan <didikunila@gmail.com>
2019-07-15 08:44:08 +0700

    Dimensi canvas dan catatan harian

--- public/js/camera.js
+++ public/js/camera.js
@@ -2,13 +2,16 @@ var player = document.getElementById('player');
 var snapshotCanvas = document.getElementById('snapshot');
 var captureButton = document.getElementById('capture');
 var videoTracks;
-var handleSuccess = function (stream) {
+var playerDim = {};
+var handleSuccess = function(stream) {
     // Attach the video stream to the video element and autoplay.
     player.srcObject = stream;
     videoTracks = stream.getVideoTracks();
+    playerDim = stream.getTracks()[0].getSettings();
+    snapshotCanvas.height = playerDim.height * 240 / playerDim.width;
 };
 
-captureButton.addEventListener('click', function () {
+captureButton.addEventListener('click', function() {
     captureButton.disabled = true;
     captureButton.style.backgroundColor = 'grey';
     var date = new Date();
@@ -21,7 +24,7 @@ captureButton.addEventListener('click', function () {
         snapshotCanvas.height);
     player.style.display = 'none';
     snapshotCanvas.style.display = 'block';
-    videoTracks.forEach(function (track) { track.stop() });
+    videoTracks.forEach(function(track) { track.stop() });
     var npm = document.getElementById('npm').value;
     var ket = document.getElementById('save').value;
     var filename = npm + '_' + thn + bln + tgl + '_' + ket + '.png';
@@ -31,16 +34,16 @@ captureButton.addEventListener('click', function () {
 
 function uploadFile(canvas, filename) {
     var storageRef = firebase.storage().ref();
-    canvas.toBlob(function (blob) {
+    canvas.toBlob(function(blob) {
         var image = new Image();
         image.src = blob;
         var uploadTask = storageRef.child('images/pkl/' + filename).put(blob);
-        uploadTask.on('state_changed', function (snapshot) {
+        uploadTask.on('state_changed', function(snapshot) {
             // Observe state change events such as progress, pause, and resume
             // See below for more detail
             var progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-            document.getElementById("progress").value=progress;
-        }, function (error) {
+            document.getElementById("progress").value = progress;
+        }, function(error) {
             // Handle unsuccessful uploads
             setButtonLabel();
             navigator.mediaDevices.getUserMedia({ video: true })
@@ -49,10 +52,10 @@ function uploadFile(canvas, filename) {
             snapshotCanvas.style.display = 'none';
             captureButton.disabled = false;
             captureButton.style.backgroundColor = 'DodgerBlue';
-        }, function () {
+        }, function() {
             // Handle successful uploads on complete
             // For instance, get the download URL: https://firebasestorage.googleapis.com/...
-            document.getElementById("success").style.display="inline";
+            document.getElementById("success").style.display = "inline";
             var downloadURL = uploadTask.snapshot.downloadURL;
             document.getElementById('imgURL').value = downloadURL;
             setButtonLabel();
@@ -61,6 +64,7 @@ function uploadFile(canvas, filename) {
     });
     console.log(uploadTask.downloadURL);
 }
+
 function leadingZero(number) {
     return number.toString().length == 1 ? '0' + number : number;
 }
